9c83cd9349d16659be5a7a27387938ea7f1cb4a6
Mika <prodomitor@gmail.com>
2020-08-03 00:14:19 +0200

    Deck interface and its implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-            PlayingCard card = new PlayingCard(Card.Suit.HEARTS, Card.Rank.FIVE);
-            PlayingCard card2 = new PlayingCard(Card.Suit.HEARTS, Card.Rank.SEVEN);
-            PlayingCard card3 = new PlayingCard(Card.Suit.HEARTS, Card.Rank.FIVE);
+        Deck deck = new StandardDeck();
+        deck.generateDeck(1);
+        deck.sortDescByRank();
+        deck.printCards();
 
-            var hash = card.hashCode();
-            var hash2 = card.hashCode();
-
-            Boolean equals = card.equals(card3);
-
-            System.out.println( card2.compareTo(card) );
+//        System.out.println(cards);
     }
 }
---
+++ src/com/company/StandardDeck.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.*;
+
+public class StandardDeck implements Deck {
+    final public List<Card> deck;
+    
+    public StandardDeck() {
+        deck = new ArrayList<>();
+    }
+
+    @Override
+    public List<Card> shuffle(List<Card> deck) {
+        Collections.shuffle(deck);
+        return deck;
+    }
+
+    @Override
+    public void sort() {
+        Collections.sort(deck);
+    }
+
+    @Override
+    public void sortDescByRank() {
+        Collections.sort(deck, (c1, c2) -> {
+        if(c1.getRank().getValue() > c2.getRank().getValue()){
+            return -1;
+        }
+        if(c2.getRank().getValue() > c1.getRank().getValue()){
+            return 1;
+        }
+        return 0;
+
+    });
+    }
+
+@Override
+    public void sortAscByRank() {
+        Collections.sort(deck, (c1, c2) -> {
+        if(c1.getRank().getValue() > c2.getRank().getValue()){
+            return 1;
+        }
+        if(c2.getRank().getValue() > c1.getRank().getValue()){
+            return -1;
+        }
+        return 0;
+
+    });
+    }
+
+    @Override
+    public List<Card> generateDeck(int numbOfDecks) {
+        if(numbOfDecks>= 1){
+            for(int i = 0; i < numbOfDecks; i++) {
+                for (Card.Suit suit : Card.Suit.values()) {
+                    for( Card.Rank rank : Card.Rank.values()){
+                        deck.add(new PlayingCard(suit, rank));
+                    }
+                }
+            }
+        }
+        return deck;
+    }
+
+    @Override
+    public void addCard(Card card) {
+        
+    }
+
+    @Override
+    public void addCards(List<Card> cards) {
+
+    }
+
+    @Override
+    public List<Card> getDeck() {
+        return deck;
+    }
+
+    @Override
+    public void printCards() {
+        deck.stream().forEach( c -> System.out.println(c));
+    }
+
+    @Override
+    public Map<Integer, List<Card>> deal(int players, int numberOfCards) {
+        return null;
+    }
+}
